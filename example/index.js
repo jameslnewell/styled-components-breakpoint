8935701fd5979b5cd672b981480f23ef0090c9a0
James Newell <james@jameslnewell.dev>
2019-09-06 17:03:38 +1000

    wip

--- example/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-const element = document.getElementById('app');
-if (element) {
-  ReactDOM.render(<App />, element);
-}
