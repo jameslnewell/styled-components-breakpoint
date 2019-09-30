7b218989820a6c6dbf5ac0f938ed3276800f7080
James Newell <james.newell@automattic.com>
2019-10-01 02:23:10 +1000

    fix examples

--- example/src/App/index.tsx
+++ example/src/App/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import * as React from 'react';
 import {ThemeProvider} from 'styled-components';
 import {
   ExampleBreakpoints,
@@ -30,7 +30,7 @@ export type AppState = {
 };
 
 export const App: React.FC = () => {
-  const [breakpoints, setBreakpoints] = useState<ExampleBreakpoints>(
+  const [breakpoints, setBreakpoints] = React.useState<ExampleBreakpoints>(
     defaultBreakpoints,
   );
 
